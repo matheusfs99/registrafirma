124a132c12a4a989d1e582c15db2251794d7f9ec
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-23 11:13:07 -0300

    create api to user

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.db import transaction
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ("create", "login"):
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request, *args, **kwargs):
+        with transaction.atomic():
+            return super().create(request, *args, **kwargs)
+
+    @action(methods=("post",), detail=False, url_path="login")
+    def login(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(username=email, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"message": "User successfully logged in"}, status=status.HTTP_200_OK)
+
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    @action(methods=("post",), detail=False, url_path="logout")
+    def logout(self, request):
+        try:
+            request.user.auth_token.delete()
+            return Response({"message": "User successfully logged out"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
