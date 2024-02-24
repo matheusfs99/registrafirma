4691ad2ab95175b2458c6ae46a98fa7bb2a6c4a4
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-23 22:24:31 -0300

    add tests

--- tests/factories/companies.py
+++ tests/factories/companies.py
@@ -1,10 +1,14 @@
 import factory
+from faker import Faker
+from utils.generate_cnpj import generate_cnpj
 from apps.companies.models import Company
 from .accounts import UserFactory
 
+faker = Faker()
 
-class CompanyFactory(factory.Factory):
-    cnpj = "29276649000111"
+
+class CompanyFactory(factory.django.DjangoModelFactory):
+    cnpj = factory.LazyFunction(generate_cnpj)
     name = "test ltda"
     fantasy_name = "test"
     user = factory.SubFactory(UserFactory)
--- tests/test_companies/conftest.py
+++ tests/test_companies/conftest.py
@@ -7,5 +7,5 @@ def company_payload(user):
         "cnpj": "29276649000111",
         "name": "test",
         "fantasy_name": "test tlda",
-        "user": user
+        "user": user.id
     }
