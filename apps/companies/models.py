026ace3ecd1d84365dbd4afcf0fbfaed2aeecfac
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-23 23:29:23 -0300

    create model employee

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -6,3 +6,8 @@ class Company(models.Model):
     name = models.CharField("Razão Social", max_length=255)
     fantasy_name = models.CharField("Nome Fantasia", max_length=255)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    company = models.ForeignKey("companies.Company", on_delete=models.CASCADE)
--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -2,6 +2,6 @@
 Model's factories
 """
 from .accounts import UserFactory
-from .companies import CompanyFactory
+from .companies import CompanyFactory, EmployeeFactory
 
-__all__ = ("UserFactory", "CompanyFactory")
+__all__ = ("UserFactory", "CompanyFactory", "EmployeeFactory")
