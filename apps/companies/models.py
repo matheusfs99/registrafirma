2e7a3274c2ee26622e10d2399dcb7cfed3bf60e6
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-25 19:25:46 -0300

    fix status choices

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -9,7 +9,7 @@ class Company(models.Model):
     cnpj = models.CharField("CNPJ", max_length=14, unique=True)
     name = models.CharField("Razão Social", max_length=255)
     fantasy_name = models.CharField("Nome Fantasia", max_length=255)
-    status = models.CharField("Status", max_length=8, choices=Status, default=Status.ACTIVE)
+    status = models.CharField("Status", max_length=8, choices=Status.choices, default=Status.ACTIVE)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     created_at = models.DateTimeField("Data de criação", auto_created=True, auto_now_add=True)
     updated_at = models.DateTimeField("Data de atualização", auto_created=True, auto_now_add=True)
