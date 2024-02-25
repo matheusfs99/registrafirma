d5fe68b77ff9b4062ed3a621a5d91b8c7c15c934
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-25 12:41:45 -0300

    create employee api

--- apps/companies/urls.py
+++ apps/companies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet
+from .views import CompanyViewSet, EmployeeViewSet
 
 router = DefaultRouter()
 router.register("company", CompanyViewSet, basename="company")
+router.register("employee", EmployeeViewSet, basename="employee")
 
 urlpatterns = [
     path("", include(router.urls)),
--- apps/companies/views.py
+++ apps/companies/views.py
@@ -1,14 +1,9 @@
-from django.db import transaction
-from django.contrib.auth import authenticate
-
 from rest_framework import viewsets, permissions
-from rest_framework.authtoken.models import Token
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 
-from .serializers import CompanySerializer
-from .models import Company
+from .serializers import CompanySerializer, EmployeeSerializer
+from .models import Company, Employee
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -22,3 +17,22 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        if self.action in ("list",):
+            company_id = self.request.query_params.get("company")
+            company = Company.objects.filter(pk=company_id, user=self.request.user).first()
+            return self.queryset.filter(company=company)
+        return self.queryset.filter(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        if "company" not in self.request.query_params:
+            return Response({"error": "query param 'company' is required"}, status=status.HTTP_400_BAD_REQUEST)
+        return super().list(request, *args, **kwargs)
+
