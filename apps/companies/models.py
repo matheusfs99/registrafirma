5b40a4eb11f24f85731b94aeb02c812c96259e07
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-02-26 09:24:42 -0300

    set fantasy_name with nullable

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -9,7 +9,7 @@ class Company(models.Model):
 
     cnpj = models.CharField("CNPJ", max_length=14, unique=True)
     name = models.CharField("Razão Social", max_length=255)
-    fantasy_name = models.CharField("Nome Fantasia", max_length=255)
+    fantasy_name = models.CharField("Nome Fantasia", max_length=255, null=True, blank=True)
     status = models.CharField("Status", max_length=11, choices=Status.choices, default=Status.ACTIVE)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     created_at = models.DateTimeField("Data de criação", auto_now_add=True, editable=False)
